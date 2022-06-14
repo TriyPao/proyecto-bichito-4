324acd9992d0655b4e0a3d8c2142f24c961e35ed
TriyPao <107255030+TriyPao@users.noreply.github.com>
2022-06-14 09:11:22 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showString("Hola!")
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0)) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.clearScreen()
+    }
+})
